0e3eea26477a4c23ad32c7d6e6711859b3e1c18b
Jon Stutters <j.stutters@ucl.ac.uk>
2019-01-11 12:14:03 +0000

    Strip trailing whitespace from setup_cmd

--- src/clustrun/unified.py
+++ src/clustrun/unified.py
@@ -7,7 +7,7 @@ def read_unified(f):
     yaml_config = yaml.safe_load(f)
     config = Config()
     config.hosts = [Host(**h) for h in yaml_config.get("hosts", list())]
-    config.setup_cmd = yaml_config.get("setup_cmd", "")
+    config.setup_cmd = yaml_config.get("setup_cmd", "").strip()
     config.cmd_tplt = yaml_config.get("cmd_tplt", "")
     config.sudo = yaml_config.get("sudo", True)
     config.tasks = yaml_config.get("tasks", list())
